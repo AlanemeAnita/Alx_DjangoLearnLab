754578d25adbeb0eb9d853bf6528199cbf7ad775
Alaneme Anita <anitaalaneme@gmail.com>
2025-09-19 13:57:00 +0100

    Task 1: Add BookList API endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -7,3 +7,10 @@ def home(request):
 urlpatterns = [
     path('', home, name='home'),
 ]
+
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()   # Get all Book records
+    serializer_class = BookSerializer  # Use the serializer to convert to JSON
